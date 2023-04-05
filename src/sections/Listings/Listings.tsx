c5b5d9478c33a071759b9d392cfcb6981feed933
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-05 15:32:38 -0400

    implement delete listing function

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { server } from "../../lib/api"
-import { ListingsData } from "./types";
+import { ListingsData, DeleteListingData, DeleteListingVariables } from "./types";
 
 const LISTINGS = `
     query Listings {
@@ -17,6 +17,14 @@ const LISTINGS = `
     }
 `;
 
+const DELETE_LISTING = `
+    mutation DeleteListing($id: ID!) {
+        deleteListing(id: $id) {
+            id
+        }
+    }
+`;
+
 interface Props {
     title: string;
 }
@@ -29,7 +37,13 @@ export const Listings = ( { title }: Props ) => {
     }
 
     const deleteListing = async () => {
-        
+        const { data } = await server.fetch<DeleteListingData, DeleteListingVariables>({
+            query: DELETE_LISTING,
+            variables: {
+                id: '64141ce7d8979c09906377b5'
+            }
+        });
+        console.log(data);
     }
     return <div>
         <h2>{title}</h2>
--- src/sections/Listings/types.ts
+++ src/sections/Listings/types.ts
@@ -18,6 +18,6 @@ export interface DeleteListingData {
     deleteListing: Listing
 }
 
-export interface DeleteListingVariable{
+export interface DeleteListingVariables{
     id: string;
 }
