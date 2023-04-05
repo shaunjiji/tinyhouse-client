7295a64e0be1459986efd639b9cafc3440863404
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-05 15:17:49 -0400

    create interfaces for delete listing data and delete listing variable

--- src/sections/Listings/types.ts
+++ src/sections/Listings/types.ts
@@ -13,3 +13,11 @@ interface Listing {
 export interface ListingsData {
     listings: Listing[]
 }
+
+export interface DeleteListingData {
+    deleteListing: Listing
+}
+
+export interface DeleteListingVariable{
+    id: string;
+}
