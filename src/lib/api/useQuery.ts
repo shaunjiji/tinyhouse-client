6d86ce96df0434ce0760c0333342c7ec881738e0
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-10 09:43:04 -0400

    introduce loading in state

--- src/lib/api/useQuery.ts
+++ src/lib/api/useQuery.ts
@@ -2,18 +2,23 @@ import { useState, useEffect, useCallback } from "react"
 import { server } from './server'
 
 interface State<TData> {
-    data: TData | null
+    data: TData | null;
+    loading: boolean;
 }
 
 export const useQuery = <TData = any> ( query: string ) => {
     const [state, setState] = useState<State<TData>>({
-        data: null
+        data: null,
+        loading: false
     });
 
    const fetch = useCallback(() => {
         const fetchApi = async () => {
+            setState({data: null, loading: true});
+
             const { data } = await server.fetch<TData>({ query });
-            setState({data});
+
+            setState({data, loading: false});
         };
         fetchApi();
     }, [query])
