10a9cd2c3589af787741f0596b1b17060a997b6d
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-14 10:54:07 -0400

    import require libraries to create new apollo client; connect apollo client to react application

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Listings } from './sections';
-
 import reportWebVitals from './reportWebVitals';
 
 
-render(<Listings title="Tinyhouse Listings"/>, document.getElementById("root"));
+const client = new ApolloClient({
+    uri: "/api",
+    cache: new InMemoryCache()
+});
+
+
+render(
+<ApolloProvider client={client}>
+<Listings title="Tinyhouse Listings"/>
+</ApolloProvider>, document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
