666b78406aef90e2948ab77743fe1002b3ef9493
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-21 14:06:31 -0400

    fix error

--- src/index.tsx
+++ src/index.tsx
@@ -16,19 +16,19 @@ const App = () => {
     return(
         <Router>
             <Routes>
-                <Route path="/" element={Home}/>
-                <Route path="/host" element={Host}/>
-                <Route path="/listing/:id" element={Listing}/>
-                <Route path="/listings/:location" element={Listings}/>
-                <Route path="/user/:id" element={User}/>
-                <Route element={NotFound}/>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/host" element={<Host/>}/>
+                <Route path="/listing/:id" element={<Listing/>}/>
+                <Route path="/listings/:location?" element={<Listings/>}/>
+                <Route path="/user/:id" element={<User/>}/>
+                <Route element={<NotFound/>}/>
             </Routes>
         </Router>
     )
 }
 render(
 <ApolloProvider client={client}>
-    <Listings title="Tinyhouse Listings"/>
+    <App/>
 </ApolloProvider>, document.getElementById("root")
 );
 
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,1 +1,6 @@
 export * from "./Listings"
+export * from "./Home"
+export * from "./Host"
+export * from "./Listing"
+export * from "./NotFound"
+export * from "./User"
